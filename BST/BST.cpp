1cfffbe41b31b5d3f451e27d739ccaf38b11a2b4
DONGYANG JIANG <pcjjdy@163.com>
2016-02-01 18:56:41 +0000

    jdy

---
+++ BST/BST.cpp
@@ -0,0 +1,78 @@
+﻿#include "stdafx.h"
+
+#include <iostream>
+using namespace std;
+//It's new version
+// jdy
+struct BstNode{
+	int data;
+	BstNode* left;
+	BstNode* right;
+
+};
+BstNode* root = NULL;
+void Insert(BstNode** tree, int data){
+
+	
+
+	if (*tree == NULL){
+		(*tree) = new BstNode();
+		(*tree)->data = data;
+		(*tree)->left = (*tree)->right = NULL;
+		
+	}
+	else if (data < (*tree)->data){
+
+		Insert(&(*tree)->left, data);
+	}
+	else {
+		Insert(&(*tree)->right, data);
+
+	}
+
+
+}
+void print_tree(BstNode* tree){
+	if (tree == NULL){
+
+	}
+	else{
+		print_tree(tree->left);
+		cout << tree->data << endl;
+		print_tree(tree->right);
+
+	}
+}
+void delete_tree(BstNode* tree){
+	if (tree == NULL){
+
+	}
+	else{
+		delete_tree(tree->left);
+
+		delete_tree(tree->right);
+		delete tree;
+
+
+	}
+}
+int main(){
+
+	Insert(&root, 15);
+	Insert(&root, 12);
+	Insert(&root, 37);
+	Insert(&root, 56);
+	Insert(&root, 64);
+	Insert(&root, 3);
+	Insert(&root, 43);
+	Insert(&root, 17);
+	Insert(&root, 27);
+
+	print_tree(root);
+
+	delete_tree(root);
+	cout << "delete completed";
+	root = NULL;
+	Insert(&root, 27);
+	print_tree(root);
+}
